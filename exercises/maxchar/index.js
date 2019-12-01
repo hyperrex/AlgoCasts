6ac15528c12fe2251432c72c21f206ce9537af21
hyperrex <mrwendell@gmail.com>
2019-12-01 14:36:44 -0800

    complete maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   const characters = {};
+//   let max = 0, hero = '';
+//   for (let char of str) {
+//     if (!characters[char]) {
+//       characters[char] = 1
+//     } else {
+//       characters[char]++
+//     }
+//   };
+//   for (let char in characters) {
+//     if (characters[char] > max) {
+//       max = characters[char]
+//       hero = char;
+//     }
+//   }
+//   return hero;
+// }
+
+function maxChar(str) {
+  const characters = {};
+  let max = 0, maxChar = '';
+  for (let char of str) {
+    characters[char] = characters[char] + 1 || 1
+  };
+  for (let char in characters) {
+    if (characters[char] > max) {
+      max = characters[char]
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
