6c040fc278f30707fe769bea9b92efec458ea922
hyperrex <mrwendell@gmail.com>
2019-12-11 18:03:21 -0800

    Solve for chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let index = 0;
+  const chunked = [];
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
